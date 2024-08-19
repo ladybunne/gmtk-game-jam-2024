bac56bdc2e7ce53926dc9b656582201ebb1a951c
Ladybunne <github@ladybunne.net>
2024-08-19 13:10:26 +1000

    Improve speed UI

--- Scenes/UI/SpeedControlUI.gd
+++ Scenes/UI/SpeedControlUI.gd
@@ -1,11 +1,5 @@
 extends CanvasLayer
 
-# And this is why I don't like @onready.
-@onready var speed_one: Button = $MarginContainer/HBoxContainer/One
-@onready var speed_two: Button = $MarginContainer/HBoxContainer/Two
-@onready var speed_three: Button = $MarginContainer/HBoxContainer/Three
-@onready var speed_four: Button = $MarginContainer/HBoxContainer/Four
-
 var active_button: Button :
 	set(p_active_button):
 		if active_button != null:
@@ -15,26 +9,31 @@ var active_button: Button :
 			active_button.disabled = true
 
 func _ready() -> void:
-	active_button = speed_one
+	active_button = %One
 	# I prefer connecting stuff like this because you can see it easily.
-	speed_one.pressed.connect(one_pressed)
-	speed_two.pressed.connect(two_pressed)
-	speed_three.pressed.connect(three_pressed)
-	speed_four.pressed.connect(four_pressed)
+	%Tenth.pressed.connect(tenth_pressed)
+	%Half.pressed.connect(half_pressed)
+	%One.pressed.connect(one_pressed)
+	%Two.pressed.connect(two_pressed)
+	%Four.pressed.connect(four_pressed)
 
 # This is terrible. Oh well!
+func tenth_pressed():
+	Engine.time_scale = 0.1
+	active_button = %Tenth
+	
+func half_pressed():
+	Engine.time_scale = 0.5
+	active_button = %Half
+
 func one_pressed():
 	Engine.time_scale = 1
-	active_button = speed_one
+	active_button = %One
 
 func two_pressed():
 	Engine.time_scale = 2
-	active_button = speed_two
-
-func three_pressed():
-	Engine.time_scale = 3
-	active_button = speed_three
+	active_button = %Two
 
 func four_pressed():
 	Engine.time_scale = 4
-	active_button = speed_four
+	active_button = %Four
