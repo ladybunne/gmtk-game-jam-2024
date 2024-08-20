233c6c0bd179c5e36a2d0ba15212227c88558bd5
Ladybunne <github@ladybunne.net>
2024-08-20 20:27:03 +1000

    Add music, integrate music, add fading music

---
+++ Scenes/FadeableAudioStreamPlayer.gd
@@ -0,0 +1,21 @@
+class_name FadeableAudioStreamPlayer extends AudioStreamPlayer
+
+@export var on: bool = false
+@export var on_volume: float = 0
+@export var off_volume: float = -40
+
+@export var fade_time: float = 2.0
+
+func _ready() -> void:
+	volume_db = on_volume if on else off_volume
+
+func _process(delta: float) -> void:
+	if on:
+		volume_db = clamp(volume_db + (on_volume - off_volume) * delta / (Engine.time_scale), off_volume, on_volume)
+	else:
+		volume_db = clamp(volume_db - (on_volume - off_volume) * delta / (Engine.time_scale), off_volume, on_volume)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_H:
+			on = !on
