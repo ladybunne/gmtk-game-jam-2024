6cdb02ac0561962611129ba3b04dd83f1bbcbc56
LarrikinDan <daniel@rona.id.au>
2024-08-20 23:19:34 +1000

    Help!

--- Scenes/UI/BaseHealthUI.gd
+++ Scenes/UI/BaseHealthUI.gd
@@ -8,7 +8,7 @@ func _process(delta: float) -> void:
 		var new_base := get_tree().get_first_node_in_group("Base")
 		if new_base is Base:
 			base = new_base
-			base.health_changed.connect(base_health_changed)	
+			base.health_changed.connect(base_health_changed)
 
 func base_health_changed(p_health: int):
 	%HealthLabel.text = str(p_health)
